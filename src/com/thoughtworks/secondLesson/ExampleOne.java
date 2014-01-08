a38f3c18567f09ab32d9c39d356b1491293d9bf8
yan wang <tianya_wy@hotmailcom>
2014-01-08 14:57:42 +0800

    ok

--- src/com/thoughtworks/secondLesson/ExampleOne.java
+++ src/com/thoughtworks/secondLesson/ExampleOne.java
@@ -1,9 +1,7 @@
 package com.thoughtworks.secondLesson;
 
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.List;
 import java.util.Vector;
 
 public class ExampleOne {
@@ -71,15 +69,7 @@ public class ExampleOne {
     }
 
     private double adjustValue(int month, int day) {
-        int totalDays = month*31+day;
-        double importantValue1 = (totalDays * 3) + 200+getDelta();
-        double importantValue2 = (totalDays * 1.5) + 400;
-
-        if((totalDays*4 - importantValue1) > 100){
-            importantValue2 = importantValue2 - 20;
-        }
-
-        return importantValue2 * 7 -importantValue1;
+        return new Gamma(this, month, day).invoke();
     }
 
     private double getOutstanding2(double initOutstanding){
@@ -102,5 +92,38 @@ public class ExampleOne {
         _PrintStream.println("********************");
         _PrintStream.println("********Customer****");
         _PrintStream.println("********************");
+    }
+
+    public static class Gamma {
+        int totalDays;
+        private ExampleOne exampleOne;
+        private double importantValue1;
+        private double importantValue2;
+
+        public Gamma(ExampleOne exampleOne, int month, int day) {
+            totalDays = month*31+day;
+            this.exampleOne = exampleOne;
+        }
+
+        public double invoke() {
+            importantValue1 = getImportValue1(totalDays);
+            importantValue2 = getImportValue2();
+            importValue();
+            return importantValue2 * 7 - importantValue1;
+        }
+
+        private double getImportValue2() {
+            return (totalDays * 1.5) + 400;
+        }
+
+        private void importValue() {
+            if((totalDays*4 - importantValue1) > 100){
+                importantValue2 = importantValue2 - 20;
+            }
+        }
+
+        private double getImportValue1(int totalDays) {
+            return (totalDays * 3) + 200+ exampleOne.getDelta();
+        }
     }
 }
